de40716bc0ddaa7843f72b0efc61479e6a8939da
Art-Julia <82719573+Art-Julia@users.noreply.github.com>
2021-05-20 19:03:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
         int initialBalance = 20;
         int balanceTopUp = 100;
         int bonus;
-        if (balanceTopUp >= 1_000) {
+        if (balanceTopUp > 1_000) {
             bonus = balanceTopUp / 100;
         } else {
             bonus = 0;
         }
-        int TotalBalance = initialBalance + balanceTopUp;
+        int totalBalance = initialBalance + balanceTopUp;
         System.out.println("Total account Balance");
-        System.out.println(TotalBalance);
+        System.out.println(totalBalance);
         System.out.println("Total Bonus");
         System.out.println(bonus);
 
